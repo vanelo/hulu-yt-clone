07912349a6e87a06cec957ec94ac609ef668d9d2
vanelo <marivane91@gmail.com>
2021-10-02 23:35:13 -0400

    set alt prop to Image and set up Thumbnail displayName

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -4,10 +4,12 @@ import { forwardRef } from "react";
 
 const Thumbnail = forwardRef(({result}, ref) => {
     const BASE_URL = "https://image.tmdb.org/t/p/original/";
+
     return (
         <div ref={ref} className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
             <Image 
                 layout="responsive"
+                alt={result.title || result.original_name}
                 src={
                     `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
                     `${BASE_URL}${result.poster_path}`
@@ -27,4 +29,6 @@ const Thumbnail = forwardRef(({result}, ref) => {
     )
 });
 
-export default Thumbnail
+Thumbnail.displayName = "Thumbnail";
+
+export default Thumbnail;
